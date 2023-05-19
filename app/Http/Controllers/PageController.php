0de809b20530432cbf551296462a1069dd82db3e
Erik Jimenez <erikedu5@gmail.com>
2023-05-19 11:19:03 -0600

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Repository;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home() {
+        return view('welcome', [
+            'repositories' => Repository::latest()->get()
+        ]);
+    }
+}
---
+++ app/Http/Controllers/RepositoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Repository;
+use Illuminate\Http\Request;
+use App\Http\Requests\RepositoryRequest;
+
+class RepositoryController extends Controller
+{
+
+    public function index()
+    {
+        return view('repositories.index', [
+            'repositories' => auth()->user()->repositories
+        ]);
+    }
+
+    public function show(Repository $repository) {
+        $this->authorize('pass', $repository);
+
+         return view('repositories.show', compact('repository'));
+    }
+
+    public function create() {
+        return view ('repositories.create');
+    }
+
+    public function store(RepositoryRequest $request)
+    {
+        $request->user()->repositories()->create($request->all());
+        return redirect()->route('repositories.index');
+    }
+
+    public function edit(Repository $repository) {
+        $this->authorize('pass', $repository);
+
+        return view('repositories.edit', compact('repository'));
+    }
+
+    public function update(RepositoryRequest $request, Repository $repository)
+    {
+        $this->authorize('pass', $repository);
+
+        $repository->update($request->all());
+        return redirect()->route('repositories.edit', $repository);
+    }
+
+    public function destroy(Repository $repository)
+    {
+        $this->authorize('pass', $repository);
+
+        $repository->delete();
+        return redirect()->route('repositories.index');
+    }
+}
